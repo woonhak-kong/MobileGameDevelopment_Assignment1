f2e28f2e9cb6df86afadc392a37b89c26eb9cc79
WoonHak Kong <woonhak.kong@georgebrown.ca>
2022-09-29 20:06:58 -0400

    MainScene is added

---
+++ Assets/[Scripts]/MainScene/MainSceneManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainSceneManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject MainPannel;
+    [SerializeField]
+    private GameObject InstructionPannel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnStartBtn()
+    {
+
+    }
+
+    public void OnInstructionBtn()
+    {
+        MainPannel.SetActive(false);
+        InstructionPannel.SetActive(true);
+    }
+
+    public void OnExitBtn()
+    {
+
+    }
+
+    public void OnBackBtn()
+    {
+        MainPannel.SetActive(true);
+        InstructionPannel.SetActive(false);
+    }
+}
