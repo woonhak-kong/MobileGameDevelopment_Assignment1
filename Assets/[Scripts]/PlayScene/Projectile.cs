71312043e4ff620a77ad3d43a676f5247679a0d1
WoonHak Kong <woonhak.kong@georgebrown.ca>
2022-10-05 21:29:15 -0400

    Canvas is modified

--- Assets/[Scripts]/PlayScene/Projectile.cs
+++ Assets/[Scripts]/PlayScene/Projectile.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class Projectile : MonoBehaviour
 {
+    [SerializeField]
+    private float projectileSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -15,5 +17,10 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(Vector2.right * projectileSpeed * Time.fixedDeltaTime);
     }
 }
